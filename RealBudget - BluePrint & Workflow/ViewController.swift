598b7f1627f44f779271d0881128aae082ba7c32
Kevin Taniguchi <kv.taniguchi@icloud.com>
2021-03-19 10:45:36 -0500

    Initial Commit

---
+++ RealBudget - BluePrint & Workflow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealBudget - BluePrint & Workflow
+//
+//  Created by Kevin Taniguchi on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
